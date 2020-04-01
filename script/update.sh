60943dc7779167d21565b0757c216fa5ec25a31f
Mathias Schilling <m@matchilling.com>
2020-04-01 08:17:12 +0200

    Make script resilient to HMRC's breaking api changes 💩

--- script/update.sh
+++ script/update.sh
@@ -37,20 +37,31 @@ read_json() {
 readonly LATEST_START_DATE=$(date -d $(read_json "$RATE_DIR/latest.json" "start"))
 readonly NEXT_YYYY=$(date -d "${LATEST_START_DATE} +1 month" "+%Y")
 readonly NEXT_MM=$(date -d "${LATEST_START_DATE} +1 month" "+%m")
-readonly HMRC_URL="http://www.hmrc.gov.uk/softwaredevelopers/rates/exrates-monthly-${NEXT_MM}${NEXT_YYYY: -2}.xml"
 
-readonly STATUS_CODE=$(curl --silent -LI -X OPTIONS "${HMRC_URL}" -o /dev/null -w '%{http_code}')
-if [ "$STATUS_CODE" != '200' ] ; then
-    echo "No rates for ${NEXT_YYYY}-${NEXT_MM} available yet."
+# HMRC breaks its URL scheme sometimes 🤷‍, so we're trying different URLS
+readonly HMRC_URL_01="http://www.hmrc.gov.uk/softwaredevelopers/rates/exrates-monthly-${NEXT_MM}${NEXT_YYYY: -2}.xml"
+readonly HMRC_URL_02="http://www.hmrc.gov.uk/softwaredevelopers/rates/exrates-monthly-${NEXT_MM}${NEXT_YYYY: -2}.XML"
+HMRC_URL="${HMRC_URL_01}"
 
-    readonly LATEST_YYYY=$(date -d "${LATEST_START_DATE}" "+%Y")
-    readonly LATEST_M=$(date -d "${LATEST_START_DATE}" "+%b")
-    echo "${README}" | \
-      sed 's/###LATEST_M_YYYY###/'"${LATEST_M} ${LATEST_YYYY}"'/g' | \
-      sed 's/###LAST_UPDATE###/'"$(date -u)"'/g' \
-      > "$PROJECT_DIR/README.md"
+readonly STATUS_CODE_01=$(curl --silent -LI -X OPTIONS "${HMRC_URL_01}" -o /dev/null -w '%{http_code}')
+if [ "$STATUS_CODE_01" != '200' ]; then
 
-    exit 0;
+    readonly STATUS_CODE_02=$(curl --silent -LI -X OPTIONS "${HMRC_URL_02}" -o /dev/null -w '%{http_code}')
+    if [ "$STATUS_CODE_02" != '200' ]
+        then
+            echo "No rates for ${NEXT_YYYY}-${NEXT_MM} available yet."
+
+            readonly LATEST_YYYY=$(date -d "${LATEST_START_DATE}" "+%Y")
+            readonly LATEST_M=$(date -d "${LATEST_START_DATE}" "+%b")
+            echo "${README}" | \
+              sed 's/###LATEST_M_YYYY###/'"${LATEST_M} ${LATEST_YYYY}"'/g' | \
+              sed 's/###LAST_UPDATE###/'"$(date -u)"'/g' \
+              > "$PROJECT_DIR/README.md"
+
+            exit 0;
+        else
+          HMRC_URL="${HMRC_URL_02}"
+    fi
 fi
 
 readonly TARGET_FILE="$RATE_DIR/${NEXT_YYYY}/${NEXT_MM}.json"
